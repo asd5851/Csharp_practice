7b018a30161163427e3dd905e4974de05c7bc739
seon jun <asd5851@naver.com>
2022-12-23 17:36:06 +0900

    [kiveiru] feat. 22.12.23

---
+++ first/Adventure_Story.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Adventure_Story
+    {
+        public string[] monster_name = new string[5] { "토끼", "늑대", "곰", "고블린", "드래곤" };
+        public string[] weapon_name = new string[5] { "나무검", "돌검", "구리검", "철검", "은검" };
+        public string[] armor_name = new string[5] { "나무갑옷", "돌 갑옷", "구리 갑옷", "철 갑옷", "은 갑옷" };
+
+        public int[,] monster_stats = new int[5,4];
+
+        
+
+        struct user
+        {
+           public int HP;
+           public int stamina;
+           public int attack;
+           public int defence;
+        }
+
+
+        // 몬스터의 스탯을 랜덤으로 배정한다.
+        // 상위 몬스터가 하위 몬스터보다 강하게 설정한다.
+        void monster_random()
+        {
+            for(int i=0;i<5; i++)
+            {
+                for(int j=0;j<4; j++)
+                {
+                    Random random= new Random();
+                    int a = i * 10;
+                    int b = (i * 10) + 10 - (j * 2);
+                    monster_stats[i,j] = random.Next(a,b);     
+                }
+                
+            }
+        }
+
+        // 타이틀 화면
+        void reference_title()
+        {
+            Console.WriteLine("**********************************************");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("");
+
+            }
+            Console.WriteLine("***************** 랜덤 모험 ******************");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("");
+
+            }
+            Console.WriteLine("\t\tPress Any Burton");
+            Console.WriteLine("**********************************************");
+
+            Console.ReadLine();
+        }
+        
+       
+        // 도망을 선택했을때
+        void run_monster(int HP, int stamina, int attack, int defence)
+        {
+            Random run_random = new Random();
+            int run = run_random.Next(1,101);
+            if(run <= 50)
+            {
+                Console.WriteLine("도망치지 못하였습니다.");
+                battle_monster(HP,stamina,attack,defence);
+            }
+            else
+            {
+                Console.WriteLine("도망에 성공하셨습니다.");
+            }
+        }
+
+        // 몬스터와 싸울때
+        void battle_monster(int HP, int stamina, int attack, int defence)
+        {
+            monster_random(); // 전역변수에 몬스터 개체와 특성
+
+            Random random = new Random();
+            int sm = random.Next(0, 5);
+            int[,] selected_monster = new int[5,4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                selected_monster[sm,i] = monster_stats[sm,i]; // 몬스터 스텟을 저장해놓고
+            }
+            Console.WriteLine("***************{0}을 만났습니다!***************", monster_name[sm]);
+
+            while (true)
+            {
+
+                // 0 : hp, 1 : stamina, 2 : attack, 3 : defence
+
+                Console.WriteLine("***************행동을 선택해 주세요***************");
+                Console.WriteLine("************ 1. 공격한다 2. 방어한다 ****************");
+                Console.WriteLine("");
+                int.TryParse(Console.ReadLine(), out int activate);
+                if (activate == 1)
+                {
+                    Console.WriteLine("***************나의 HP : {0}***************", HP);
+                    Console.WriteLine("***************{0}의 HP : {1}***************", monster_name[sm], monster_stats[sm,0]);
+                    Console.WriteLine("***************나의 공격력 : {0}***************", attack);
+                    Console.WriteLine("***************{0}의 공격력 : {1}***************", monster_name[sm], monster_stats[sm, 2]);
+
+
+                    Console.WriteLine("★★★★★★ 나의 공격! ★★★★★★");
+                    if (monster_stats[sm,3] >= attack) // 고블린의 방어력이 내 공격보다 높다면
+                    {
+                        monster_stats[sm,0]--;
+
+                    }
+                    else
+                    {
+                        monster_stats[sm,0] = monster_stats[sm,0] - attack + monster_stats[sm,3];
+
+
+                    }
+                    Console.WriteLine("나의 남은 HP : {0}", HP);
+                    Console.WriteLine("{0}의 남은 HP : {1}", monster_name[sm], monster_stats[sm,0]);
+
+                    Console.WriteLine("★★★★★★ {0}의 공격! ★★★★★★", monster_name[sm]);
+                    if (monster_stats[sm,2] <= defence)
+                    {
+                        HP--;
+                    }
+                    else
+                    {
+                        HP = HP - monster_stats[sm,2] + defence;
+                    }
+                    Console.WriteLine("나의 남은 HP : {0}", HP);
+                    Console.WriteLine("{0}의 남은 HP : {1}", monster_name[sm], monster_stats[sm,0]);
+                    Console.ReadLine();
+
+                }
+
+                else if (activate == 2)
+                {
+                    Console.WriteLine("***************{0}의 공격을 막았습니다.***************", monster_name[sm]);
+                }
+                else
+                {
+                    Console.WriteLine("***************잘못입력하셨습니다. 다시 입력해 주세요***************");
+
+                }
+
+                if (HP <= 0)
+                {
+                    Console.WriteLine("***************죽었습니다***************");
+                    break;
+                }
+                else if (monster_stats[sm,0] <= 0)
+                {
+                    Console.WriteLine("***************승리하셨습니다***************");
+
+                    break;
+                }
+            }
+            Console.WriteLine("");
+
+
+
+        }
+
+        // 몬스터와 만났을 때
+        void meet_monster(int HP, int stamina, int attack, int defence)
+        {
+            Console.WriteLine("*************** 몬스터를 만났습니다! ***************");
+            Console.WriteLine("   ( 몬스터 종류, 스탯은 모두 랜덤 입니다 )");
+
+            Console.WriteLine("\t\t행동을 결정해 주세요");
+            Console.WriteLine("");
+
+            Console.WriteLine("\t1. 싸운다\t 2. 도망간다");
+            int.TryParse(Console.ReadLine(), out int activate);
+            if(activate == 1)
+            {
+                battle_monster(HP,stamina, attack, defence);
+            }
+            else if(activate == 2)
+            {
+                run_monster(HP, stamina,attack,defence);
+            }
+            else
+            {
+                Console.WriteLine("\t잘못입력하셨습니다. 다시 입력해 주세요");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            var ms = new Adventure_Story();
+            ms.reference_title();
+            Console.Write("\t   이름을 입력해 주세요 : ");
+            string name = Console.ReadLine();
+
+            Console.Write("\t 능력치는 랜덤으로 분배 합니다.");
+            Console.WriteLine();
+            Random random = new Random();
+
+            user player;
+            player.HP = random.Next(1, 100 + 1);
+            player.stamina = random.Next(1, 100 + 1);
+            player.attack = random.Next(1, 15 + 1);
+            player.defence = random.Next(1, 15 + 1);
+
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("이름\t : {0}", name);
+            Console.WriteLine("체력\t : {0}", player.HP);
+            Console.WriteLine("스태미나 : {0}", player.stamina);
+            Console.WriteLine("공격력\t : {0}", player.attack);
+            Console.WriteLine("방어력\t : {0}", player.defence);
+            Console.WriteLine("**********************************************");
+            
+
+            Console.WriteLine("") ;
+
+            Console.WriteLine("메인 퀘스트 : ") ;
+            Console.WriteLine("랜덤마을의 랜덤용사여 랜덤마왕을 무찌르고 랜덤공주를 구출하라!");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("랜덤 용사가 선택마을에 도착하였다") ;
+            Console.WriteLine("??? : 용사님 저희마을을 괴롭히고 있는 몬스터를 혼내주세요");
+            Console.WriteLine("***************1. 수락한다 2. 거절한다***************");
+            
+            string[] weapon_name = new string[5] { "나무검", "돌검", "구리검", "철검", "은검" };
+            string[] armor_name = new string[5] { "나무갑옷", "돌 갑옷", "구리 갑옷", "철 갑옷", "은 갑옷" };
+            int[] weapon_stats = new int[5] { 5, 10, 15, 20, 25 };
+            int[] armor_stats = new int[5] { 5, 10, 15, 20, 25 };
+            
+            while (true)
+            {
+                int.TryParse(Console.ReadLine(), out int message);
+                if (message == 1)
+                {
+                    Console.WriteLine("네 저 랜덤용사가 몬스터를 무찌르겠습니다!");
+                    Console.ReadLine();
+
+                    Console.WriteLine("(랜덤)보상을 받았습니다.");
+                    Random random1 = new Random();
+                    int take = random1.Next(0, 4 + 1);
+                    Console.WriteLine("{0}을 받았습니다", weapon_name[take]);
+                    Console.WriteLine("공격력이 {0} 만큼 올랐습니다.", weapon_stats[take]);
+                    player.attack = player.attack + weapon_stats[take];
+                    break;
+
+                }
+                else if (message == 2)
+                {
+                    Console.WriteLine("싫은데요");
+                    Console.ReadLine();
+
+                    Console.WriteLine("보상을 받지 못했습니다.");
+                    Console.WriteLine("스태미너가 {0}만큼 내려갔습니다.", 5);
+                    player.stamina = player.stamina - 5;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("잘못 입력 하셨습니다.");
+                    Console.WriteLine("다시 입력해 주세요");
+                }
+            }
+         
+            Console.ReadLine();
+            ms.meet_monster(player.HP, player.stamina, player.attack, player.defence); // 몬스터 만나는 함수
+           
+
+            //Console.Write("")
+
+        }
+    }
+}
---
+++ first/Program6.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Program6
+    {
+        static int []sorted = new int[10];
+        //void conquer(int start, int end, int mid, int []arr)
+        //{
+        //    int i = start;
+        //    int j = mid + 1;
+        //    int k = start;
+        //    while(i<=mid && j<=end)
+        //    {
+        //        if (arr[i] <= arr[j]) // 3 1 2 5
+        //        {
+        //            Console.WriteLine("1. arr[i] = {0} vs arr[j] = {1} ",arr[i], arr[j]);
+        //            sorted[k] = arr[i];
+        //            i++;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("2. arr[i] = {0} vs arr[j] = {1} ", arr[i], arr[j]);
+
+        //            sorted[k] = arr[j];
+        //            j++;
+        //        }
+        //        k++;
+        //    }
+
+        //    if (i > mid)
+        //    {
+        //        for(int index=j; index <= end; index++) {
+        //            sorted[k] = arr[index];
+        //            k++;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        for(int index = i; index <=mid ;index++) {
+        //            sorted[k] = arr[index];
+        //            k++;
+        //        }
+        //    }
+
+        //    for(int index = start; index <= end ; index++)
+        //    {
+        //        arr[index] = sorted[index];
+        //    }
+        //}
+        //void merge_divide(int start, int end, int []arr)
+        //{
+        //    if(start < end)
+        //    {
+        //        int mid = (start + end) / 2;
+        //        merge_divide(start, mid, arr);
+        //        merge_divide(mid + 1, end, arr);
+
+        //        conquer(start, end, mid, arr);
+        //    }
+        //}
+        static void Main(string[] args)
+        {
+
+            /*
+             *  1. 사용자로부터 2개의 문자열을 읽어서 같은지 다른지 화면에 출력하는 프로그램 작성
+             */
+            Console.Write("입력하세요 : ");
+            string first = Console.ReadLine();
+            Console.Write("입력하세요 : ");
+            string second = Console.ReadLine();
+
+            bool flag = true;
+            if(first.Length == second.Length)
+            {
+                for (int i = 0; i < first.Length; i++)
+                {
+                    if (first[i] != second[i])
+                    {
+                        flag = false;
+                        break;
+                    }
+
+                }
+            }
+            else
+            {
+                flag = false;
+            }
+            
+
+            if (flag)
+            {
+                Console.WriteLine("두개의 문자열은 같습니다.");
+            }
+            else
+            {
+                Console.WriteLine("두개의 문자열은 다릅니다.");
+            }
+            /*
+             * 5개의 음료(콜라,물,스프,주스,커피)를 판매하는 자판기 머신을 구현하기
+             * 사용자가 1~5사이의 숫자입력
+             * 선택한 음료 출력
+             */
+            Console.WriteLine("1.콜라 2.물 3.스프라티 4.주스 5.커피");
+
+            string[] bev_name = new string[5] { "콜라","물","스프라티","주스","커피"};
+            Console.Write("숫자를 입력하세요 : ");
+            int.TryParse(Console.ReadLine(), out int bev);
+            if(1<=bev && bev <= 5)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (bev == i + 1)
+                    {
+                        Console.WriteLine("{0}을 선택하셨습니다", bev_name[i]);
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("잘못 누르셨습니다.");
+            }
+            
+
+            /*
+             * 배열 days[]를 아래와 같이 초기화 하고 배열 요소의 값을 다음과 같이 출력하는 프로그램 작성
+             * days[] -> 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+             * ex) 1월은 31일까징비니다.
+             *      2월은 29일까지입니다
+             */
+            int[] days = new int[12] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            for(int i=0;i<12; i++)
+            {
+                Console.WriteLine("{0}월은 {1}일 까지입니다.", i + 1, days[i]);
+            }
+
+            //int[] arr = new int[10] { 3, 1, 2, 5, 4, 6, 7, 8, 10, 9 };
+
+            //var mc = new Program6();
+
+            //mc.merge_divide(0, 9, arr);
+            //for(int i=0;i<9; i++)
+            //{
+            //    Console.Write("{0} ", sorted[i]);
+
+            //}
+
+        }
+    }
+}
+/*
+ * doxygen 클래스 다이어그램을 뽑아줌
+ * commit -m "[닉네임] feat(내가 개발한 내용이 영향을 끼칠 수 있는 범위) : 내가 한 일(퀘스트창에 알림 기능 추가)"
+ * feat : 기능추가
+ * fix : 수정
+ * design : 디자인 변경
+ * !HOTFIX : 치명적인 버그고쳐야함
+ * Style : 코드 포맷 변경
+ * Comment : 필요한 주석 추가 및 변경
+ * 등등
+ * 
+ * fetch로 내려받아서 소스트리로 항상 보기
+ * 
+ * 레퍼런스 게임 작업 해보면서 내가 지금까지 배운 것들로 무엇을 어디까지 구현 할 수 있을지
+ * 마인드맵으로 그려보기
+ * - 타이틀 씬
+ * - 선택지를 포함한 이벤트 -> 유저가 뭔가 선택 가능한 형태
+ * - 보상 or 패널티를 얻을 수 있는 이벤트 -> 유저가 골드 얻거나 아이템을 얻거나 스탯치가 떨어지거나 하는 형태
+ * - 전투 씬
+ */
